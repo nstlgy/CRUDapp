9beb28fab6c91cf14f259c8e2d9d49d3911214ec
nstlgy <yvsh7.btw@gmail.com>
2024-10-24 22:41:20 +0530

    added show routes and UUID npm package for generating id's dynamically

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const { v4: uuid } = require("uuid");
 
 // App Configuration
 const app = express();
@@ -16,18 +17,22 @@ app.set("views", path.join(__dirname, "/views"));
 // Fake Database
 let comments = [
   {
+    id: uuid(),
     username: "Todd",
     comment: "lol that is so funny!",
   },
   {
+    id: uuid(),
     username: "Skyler",
     comment: "I like to go birdwatching with my dog",
   },
   {
+    id: uuid(),
     username: "Sk8erBoi",
     comment: "Plz delete your account, Todd",
   },
   {
+    id: uuid(),
     username: "onlysayswoof",
     comment: "woof woof woof",
   },
@@ -48,8 +53,15 @@ app.get("/comments/new", (req, res) => {
 });
 
 app.post("/comments", (req, res) => {
-  console.log(req.body);
-  res.send("works");
+  const { username, comment } = req.body;
+  comments.push({ username, comment });
+  res.render("comments/index", { comments });
+});
+
+app.get("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render("comments/show", { comment });
 });
 
 // Server
